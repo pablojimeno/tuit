17d2188bd9aa8ad22a005170080f26e9bd86b07e
Pablo Jimeno <pdjimeno@gmail.com>
2011-06-27 18:55:00 +0200

    Initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tuit::Application.config.secret_token = '733e75f1eface2852f3a8ffe16a70970794f0f449527896ea47973eb8ee21b572422ca6adf4b956f100955e72989960f63a555437715ca37dbf5060d80f5e2e1'
